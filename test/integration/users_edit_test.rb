9a6226bf7c541d271b6d6bea2309f1cc135d587a
Hara Daisuke <wildlife402@gmail.com>
2019-03-30 13:21:03 +0900

    10-2-3完了。テストコードの書き方がよくわからない・・・

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,20 +17,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert","The form contains 4 errors."
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
-    name = "Foo Bar"
+    log_in_as(@user)
+    name  = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user),params:{user: {name: name,
-                           email: email,
-                           password:      "",
-                           password_confirmation:""}}
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal name, @user.name
+    assert_equal name,  @user.name
     assert_equal email, @user.email
   end
 end
