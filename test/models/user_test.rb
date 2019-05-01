97729809c7dd831b68220e5d4aa1003017585102
daisuke-hara0515 <wildlife402@gmail.com>
2019-05-01 11:29:23 +0900

    14-3-1完了。ステータスフィードのテスト確認だけ

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -94,4 +94,18 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    #フォローしているユーザーの投稿を確認
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    #フォローしていないユーザーの投稿を確認
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
